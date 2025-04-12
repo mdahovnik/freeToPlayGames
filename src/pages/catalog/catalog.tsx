e773d171adb134de33a8846218403cf90cdf49c9
Maksim Dahovnik <dahovnik@gmail.com>
2025-04-13 00:14:53 +0200

    remove back button

--- src/pages/catalog/catalog.tsx
+++ src/pages/catalog/catalog.tsx
@@ -1,4 +1,4 @@
-import {Card, Flex} from "antd";
+import {Card, Descriptions, Flex} from "antd";
 import {TCatalog} from "./type.ts";
 import {FC} from "react";
 import {Link} from "react-router-dom";
@@ -7,23 +7,28 @@ export const Catalog: FC<TCatalog> = ({games}) => {
   return (
     <>
       {
-        <Flex gap={'small'}>
-          {
-            games.map((game) => (
-              <Link key={game.id} to={`/game/${game.id}`}>
-                <Card
-                  cover={<img alt={game.short_description} src={game.thumbnail}></img>}
-                  hoverable onClick={() => {
-                  console.log('click')
-                }}>
-                  <p>{game.title}</p>
-                  <p>{game.release_date}</p>
-                  <p>{game.publisher}</p>
-                  <p>{game.genre}</p>
-                </Card>
-              </Link>
-            ))}
-        </Flex>
+        <div style={{margin: 'auto 0'}}>
+
+          <Flex gap={'small'} wrap justify={'center'}>
+            {
+              games.map((game) => (
+                <Link key={game.id} to={`/game/${game.id}`} style={{width: '20em'}}>
+                  <Card cover={<img alt={game.short_description} src={game.thumbnail}></img>}
+                        hoverable
+                        onClick={() => {
+                          console.log('click')
+                        }}>
+                    <Descriptions title={game.title} column={1} size={'small'}>
+                      <Descriptions.Item label={'Release date'}>{game.release_date}</Descriptions.Item>
+                      <Descriptions.Item label={'Publisher'}>{game.publisher}</Descriptions.Item>
+                      <Descriptions.Item label={'Developer'}>{game.developer}</Descriptions.Item>
+                      <Descriptions.Item label={'Genre'}>{game.genre}</Descriptions.Item>
+                    </Descriptions>
+                  </Card>
+                </Link>
+              ))}
+          </Flex>
+        </div>
       }
     </>
   )
--- src/pages/gameDetails/gameDetails.tsx
+++ src/pages/gameDetails/gameDetails.tsx
@@ -1,27 +1,30 @@
-import {Card, Flex, Image} from "antd";
+import {Card, Carousel, Descriptions, Image} from "antd";
 import {FC} from "react";
 import {TGameDetails} from "./type.ts";
 
 export const GameDetails: FC<TGameDetails> = ({details}) => {
 
-
   return (
     <>
-      <Card cover={<img src={details.thumbnail}/>}>
-        <p>{details.title}</p>
-        <p>Release date: {details.release_date}</p>
-        <p>Publisher: {details.publisher}</p>
-        <p>Genre: {details.genre}</p>
-        <p>Developer: {details.developer}</p>
-        <p>{details.description}</p>
+      <Card cover={<img alt={details.title} src={details.thumbnail}/>}>
+        <Descriptions title={details.title} column={1} size={'small'}>
+          <Descriptions.Item label={'Release date'}>{details.release_date}</Descriptions.Item>
+          <Descriptions.Item label={'Publisher'}>{details.publisher}</Descriptions.Item>
+          <Descriptions.Item label={'Genre'}>{details.genre}</Descriptions.Item>
+          <Descriptions.Item label={'Developer'}>{details.developer}</Descriptions.Item>
+          <Descriptions.Item label={'Description'}
+                             styles={{content: {textAlign: "start"}}}>
+            {details.description}
+          </Descriptions.Item>
+        </Descriptions>
+        <Carousel arrows infinite={true}>
+          {
+            details.screenshots.map((item) => (
+              <Image src={item.image}></Image>
+            ))
+          }
+        </Carousel>
       </Card>
-      <Flex gap={"small"}>
-        {
-          details.screenshots.map((item) => (
-            <Image src={item.image}></Image>
-          ))
-        }
-      </Flex>
     </>
   )
 }
