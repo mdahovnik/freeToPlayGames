3eec4b1e9eac79227c00bc34a892ab01c8679c29
Maksim Dahovnik <dahovnik@gmail.com>
2025-05-07 22:36:32 +0200

    feat: paginationSlice

---
+++ src/services/slices/paginationSlice/paginationSlice.ts
@@ -0,0 +1,38 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+type TPagination = {
+  currentPage: number;
+  pageSize: number;
+}
+
+const initialState: TPagination = {
+  currentPage: 1,
+  pageSize: 20,
+}
+
+const paginationSlice = createSlice({
+  name: 'pagination',
+  initialState,
+  reducers: {
+    setCurrentPage: (state, action: PayloadAction<number>) => {
+      state.currentPage = action.payload;
+    },
+    setPageSize: (state, action: PayloadAction<number>) => {
+      state.pageSize = action.payload;
+    },
+    resetPagination: (state) => {
+      state.currentPage = initialState.currentPage;
+      state.pageSize = initialState.pageSize;
+    }
+  },
+  extraReducers: () => {
+  },
+  selectors: {
+    selectCurrentPage: (state) => state.currentPage,
+    selectPageSize: (state) => state.currentPage
+  }
+})
+
+export const {setCurrentPage, setPageSize, resetPagination} = paginationSlice.actions
+export const {selectPageSize, selectCurrentPage} = paginationSlice.selectors;
+export default paginationSlice.reducer;
